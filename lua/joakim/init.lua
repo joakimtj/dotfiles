0b33b99e783f23518d92d909bfde19635a6d8959
joakimtj <joakimtj@hiof.no>
2025-02-28 09:59:16 +0100

    autocmd to make bgs transparent
    this was a claude cook

--- lua/joakim/init.lua
+++ lua/joakim/init.lua
@@ -1,5 +1,24 @@
 require("joakim.options")
 require("joakim.keymap")
 require("joakim.lazy")
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "*",
+  callback = function()
+    -- Make backgrounds transparent
+    local groups = {
+      "Normal", "NormalFloat", "NormalNC",
+      "StatusLine", "StatusLineNC", 
+      "TabLine", "TabLineFill", "TabLineSel",
+      "SignColumn", "LineNr", "CursorLineNr",
+      "VertSplit", "WinSeparator"
+    }
+    
+    for _, group in ipairs(groups) do
+      vim.api.nvim_set_hl(0, group, { bg = "NONE", ctermbg = "NONE" })
+    end
+  end,
+})
+
 vim.cmd("colorscheme nightfox")
 print("Hello from Joakim")
